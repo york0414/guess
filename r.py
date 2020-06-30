1c3878afdc219015a163d2e9d598253a4ac85b26
York Liu <york0414@gmail.com>
2020-06-30 22:06:57 +0800

    second version

--- r.py
+++ r.py
@@ -2,7 +2,14 @@ import random
 
 r = random.randint (1, 100)
 while True:
-	num = input('請輸入數字: ')
+	num = input('請猜數字: ')
 	num = int(num)
-	
+	if num == r:
+		print('恭喜你猜中了！')
+		break
+	elif num > r:
+		print('比答案大了喔')
+
+	elif num < r:
+		print('比答案小了喔!')
 
