6f89c0efd9c89d431e796d7ee037ec5e69ff0566
York Liu <york0414@gmail.com>
2020-06-30 22:40:28 +0800

    new updated with count

--- r.py
+++ r.py
@@ -1,6 +1,10 @@
 import random
+start = input('請輸入數字範圍開始值')
+end = input('請輸入數字範圍結束值')
+start = int(start)
+end = int(end)
 
-r = random.randint (1, 100)
+r = random.randint (start, end)
 count = 0
 while True:
 	count  += 1
